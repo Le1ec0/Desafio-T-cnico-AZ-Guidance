e4a5423a291cbbc6d2919426aeff3babce02f550
Le1ec0 <leandroizidoro@hotmail.com>
2024-10-02 15:13:48 -0300

    Alterado o mapeamento da entidade para a tabela correta

--- DesafioTecnicoAPI/Context/AppDbContext.cs
+++ DesafioTecnicoAPI/Context/AppDbContext.cs
@@ -1,3 +1,4 @@
+using DesafioTecnicoAZGuidance.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
@@ -5,12 +6,26 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<PermissaoCliente> PermissoesClientes { get; set; }
+    public DbSet<PermissaoTipo> PermissaoTipos { get; set; }
+    public DbSet<PermissaoEnviarPara> PermissaoEnviarPara { get; set; }
+    public DbSet<PermissaoFormaEnvio> PermissaoFormaEnvio { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // Mapeia a entidade PermissaoCliente para a tabela Permissao_Cliente
         modelBuilder.Entity<PermissaoCliente>()
-            .ToTable("Permissao_Cliente"); // Nome da tabela no banco de dados
+            .ToTable("Permissao_Cliente");
+
+        // Mapeia a entidade PermissaoTipo para a tabela Permissão_Tipo
+        modelBuilder.Entity<PermissaoTipo>()
+            .ToTable("Permissão_Tipo");
+
+        // Mapeia a entidade PermissaoEnviarPara para a tabela Permissão_Enviar_Para
+        modelBuilder.Entity<PermissaoEnviarPara>()
+            .ToTable("Permissão_Enviar_Para");
+
+        // Mapeia a entidade PermissaoFormaEnvio para a tabela Permissão_Forma_Envio
+        modelBuilder.Entity<PermissaoFormaEnvio>()
+            .ToTable("Permissão_Forma_Envio");
     }
 }
-
