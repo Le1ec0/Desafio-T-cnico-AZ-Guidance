ac8c3eccff3e451ddbea2c6145ce0c6a83a18b62
Le1ec0 <leandroizidoro@hotmail.com>
2024-10-02 15:31:53 -0300

    Corrigido classe para DTO

--- DesafioTecnicoBlazor/Models/PermissaoCliente.cs
+++ DesafioTecnicoBlazor/Models/PermissaoCliente.cs
@@ -1,11 +1,21 @@
-namespace DesafioTecnicoAZGuidance.Models
+using System.Text.Json.Serialization;
+
+public class PermissaoCliente
 {
-    public class PermissaoCliente
+    public int ClienteID { get; set; }
+
+    // Propriedade para o valor booleano
+    public bool Permitido { get; set; }
+
+    // Propriedade para o valor que vem do JSON
+    [JsonPropertyName("permitido")]
+    public string PermitidoString
     {
-        public int ClienteID { get; set; }
-        public bool Permitido { get; set; }
-        public int TipoEmailID { get; set; }
-        public int EnviarParaID { get; set; }
-        public int FormaEnvioRmID { get; set; }
+        get => Permitido ? "Sim" : "Não";
+        set => Permitido = value == "Sim";
     }
+
+    public int TipoEmailID { get; set; }
+    public int EnviarParaID { get; set; }
+    public int FormaEnvioRmID { get; set; }
 }
