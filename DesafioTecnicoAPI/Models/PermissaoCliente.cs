e3f23ef140b717822998011d901009dd6d690cac
Le1ec0 <leandroizidoro@hotmail.com>
2024-10-02 15:15:08 -0300

    Adicionado DTO

--- DesafioTecnicoAPI/Models/PermissaoCliente.cs
+++ DesafioTecnicoAPI/Models/PermissaoCliente.cs
@@ -1,12 +1,47 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class PermissaoCliente
+namespace DesafioTecnicoAZGuidance.Models
 {
-    [Key]
-    public int ClienteID { get; set; }
+    public class PermissaoCliente
+    {
+        [Key]
+        public int ClienteID { get; set; }
+        public bool Permitido { get; set; }
 
-    public bool Permitido { get; set; }
-    public int TipoEmailID { get; set; }
-    public int EnviarParaID { get; set; }
-    public int FormaEnvioRmID { get; set; }
+        public int TipoEmailID { get; set; }
+        public int EnviarParaID { get; set; }
+        public int FormaEnvioRmID { get; set; }
+
+        // Propriedades de navegação
+        [ForeignKey("TipoEmailID")]
+        public virtual PermissaoTipo TipoEmail { get; set; }
+
+        [ForeignKey("EnviarParaID")]
+        public virtual PermissaoEnviarPara EnviarPara { get; set; }
+
+        [ForeignKey("FormaEnvioRmID")]
+        public virtual PermissaoFormaEnvio FormaEnvio { get; set; }
+    }
+
+    public class PermissaoTipo
+    {
+        [Key]
+        public int TipoEmailID { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class PermissaoEnviarPara
+    {
+        [Key]
+        public int EnviarParaID { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class PermissaoFormaEnvio
+    {
+        [Key]
+        public int FormaEnvioRmID { get; set; }
+        public string Descricao { get; set; }
+    }
 }
