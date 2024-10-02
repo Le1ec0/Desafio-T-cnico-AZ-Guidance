c15bc685b0e03282a2d16ff439c5a4fa4a6b826c
Le1ec0 <leandroizidoro@hotmail.com>
2024-10-02 14:26:01 -0300

    Corrigido mapeamento do banco

--- DesafioTecnicoAPI/Context/AppDbContext.cs
+++ DesafioTecnicoAPI/Context/AppDbContext.cs
@@ -5,4 +5,12 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<PermissaoCliente> PermissoesClientes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Mapeia a entidade PermissaoCliente para a tabela Permissao_Cliente
+        modelBuilder.Entity<PermissaoCliente>()
+            .ToTable("Permissao_Cliente"); // Nome da tabela no banco de dados
+    }
 }
+
